f42fc4c65194faabe9e064e6557e6cb86a4a9408
hwei115j <hwei115j@gmail.com>
2018-04-01 01:23:51 +0800

    now

--- now/q10004.c
+++ now/q10004.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX 200
+#define MAX 201
 
 int main()
 {
@@ -9,13 +9,41 @@ int main()
     {
         int a, b;
         int i, j;
-        int arr[MAX] = {a};
+        int flag = 0;
+        int arr[MAX] = {'a'};
         int map[MAX][MAX] = {};
         
         while(m-- && scanf("%d%d", &a, &b))
             map[a][b] = map[b][a] = 1;
-        for(i = 0; i < MAX; i++)
-            for(j = 1; j < MAX; j++)
-                arr[i] = map[i][j];
+
+        for(i = 0; i < MAX-1 && !flag; i++)
+        {
+            if(arr[i] == 'a')
+            {
+                for(j = i+1; j < MAX; j++)
+                    if(map[i][j] && arr[j] == 'a')
+                        flag++;
+                    else if(map[i][j])
+                        arr[j] = 'b';
+            }
+            if(arr[i] == 'b')
+            {
+                for(j = i+1; j < MAX; j++)
+                    if(map[i][j] && arr[j] == 'b')
+                        flag++;
+                    else if(map[i][j])
+                        arr[j] = 'a';
+            }
+        }
+
+        printf((flag > 0)?"NOT BICOLORABLE.\n":"BICOLORABLE.\n");
+        /*for(i = 0; i < 10; i++)
+        {
+            for(j = 0; j < 10; j++)
+                printf("%d ", map[i][j]);
+            printf("\n");
+        }*/
     }
+
+    return 0;
 }
